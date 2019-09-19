1e9c82ec86a4aa793af40c337883fba4ebc25321
Atsushi Nakamura <nuits.jp@live.jp>
2019-09-20 07:04:53 +0900

    add sample

---
+++ SampleApp/RedSheeps.Wpf.Infragistics.MaterialDesignThemes/ResourceSets/DataPresenter/DataPresenter_ResourceSets.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Infragistics.Windows.Themes;
+
+// ReSharper disable once CheckNamespace
+namespace RedSheeps.Wpf.Infragistics.MaterialDesignThemes.ResourceSets.Indigo
+{
+    public class DataPresenter : DataPresenterResourceSet<Locator>
+    {
+        public static DataPresenter Instance { get; } = new DataPresenter();
+
+        public DataPresenter()
+        {
+
+        }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class Locator : ResourceSetLocator
+    {
+        public override Assembly Assembly => this.GetType().Assembly;
+        public override string Theme => "Indigo";
+        public override string Grouping => DataPresenterGeneric.Instance.Grouping;
+        public override string ResourcePath => @"ResourceSets\DataPresenter\DataPresenterRoyalLight.xaml";
+    }
+}
---
+++ SampleApp/RedSheeps.Wpf.Infragistics.MaterialDesignThemes/ThemesIndigoAssemblyResourceSetLoader.cs
@@ -0,0 +1,33 @@
+﻿using Infragistics.Windows.Themes;
+
+// ReSharper disable once CheckNamespace
+namespace RedSheeps.Wpf.Infragistics.MaterialDesignThemes.ResourceSets.Indigo
+{
+    public class ThemesIndigoAssemblyResourceSetLoader : AssemblyResourceSetLoader
+    {
+        protected override void OnRegisterResourceSets(string groupingName)
+        {
+            if (groupingName == null)
+            {
+                //ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.Primitives.Instance);
+                //ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.Editors.Instance);
+                ThemesIndigoAssemblyResourceSetLoader.RegisterResourceSet(DataPresenter.Instance);
+            }
+            else
+            {
+                switch (groupingName)
+                {
+                    case "Primitives":
+                        //ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.Primitives.Instance);
+                        break;
+                    case "DataPresenter":
+                        ThemesIndigoAssemblyResourceSetLoader.RegisterResourceSet(DataPresenter.Instance);
+                        break;
+                    case "Editors":
+                        //ThemesOrangePeelAssemblyResourceSetLoader.RegisterResourceSet(OrangePeel.Editors.Instance);
+                        break;
+                }
+            }
+        }
+    }
+}
